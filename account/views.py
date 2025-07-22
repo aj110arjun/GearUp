100f4bc7a47d0466852ee6237ae641db0902e859
Arjun <aj110arjun@gmail.com>
2025-07-22 19:16:20 +0530

    Login fixed

--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
 
 
+@never_cache
 @login_required(login_url='login')
 def account_info(request):
 	return render(request, 'registration/account.html')
