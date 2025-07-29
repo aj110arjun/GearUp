8aa516bc71834d3ded85548a7663c4f705cb01a8
Arjun <aj110arjun@gmail.com>
2025-07-29 10:58:51 +0530

    increment decrement

--- cart/urls.py
+++ cart/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('view/', views.view_cart, name='view_cart'),
     path('add-to-cart/<uuid:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
-    
+    path('cart/increment/<int:item_id>/', views.increment_quantity, name='increment_quantity'),
+    path('cart/decrement/<int:item_id>/', views.decrement_quantity, name='decrement_quantity'),
 ]
