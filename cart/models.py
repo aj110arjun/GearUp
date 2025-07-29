c38ad9f647668109f5a83b7c4757ab130d056975
Arjun <aj110arjun@gmail.com>
2025-07-29 10:11:24 +0530

    cart added

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from products.models import Product
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    session_key = models.CharField(max_length=40, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def total_price(self):
+        return sum(item.total_price() for item in self.items.all())
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def total_price(self):
+        return self.product.price * self.quantity
