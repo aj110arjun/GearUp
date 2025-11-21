e74157dd90659e8e044919838e6d1b4006075c94
Arjun AJ <aj110arjun@gmail.com>
2025-11-21 22:43:56 +0530

    fix: authentication bugs fixed

--- auths/views.py
+++ auths/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserCreationForm
 
 
 def signup(request):
-    return render(request, 'user/auth/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'user/auth/signup.html', context)
 
 
 def signin(request):
