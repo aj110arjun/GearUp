7704ebdb1ead13ba585cd0d21f042b96d9c7aa4b
Arjun AJ <aj110arjun@gmail.com>
2025-11-22 09:57:06 +0530

    feat: otp verification on signup added

--- auths/urls.py
+++ auths/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
+    path('verify-otp/', views.verify_otp, name='verify_otp'),
+    path('resend-otp/', views.resend_otp, name='resend_otp'),
 ]
