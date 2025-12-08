715ed8282586394b3930e05d73423ee79aa2485e
Arjun AJ <aj110arjun@gmail.com>
2025-12-08 13:54:51 +0530

    fix(reviews):profile image on reviews addes

--- common/products/templatetags/custom_filters.py
+++ common/products/templatetags/custom_filters.py
@@ -1,28 +1,14 @@
-# common/templatetags/custom_filters.py
+# common/templatetags/custom_filters.py (or your app's templatetags directory)
 from django import template
 
 register = template.Library()
 
-@register.filter(name='get_item')
+@register.filter
 def get_item(dictionary, key):
-    """Get an item from a dictionary using a key."""
-    if dictionary and key in dictionary:
-        return dictionary.get(key)
-    return None
+    """Get an item from a dictionary by key"""
+    return dictionary.get(str(key))
 
-# Alternative: If you want to access nested dictionaries
-@register.filter(name='get_item')
-def get_item(dictionary, key_path):
-    """Get an item from a dictionary using dot notation for nested keys."""
-    if not dictionary:
-        return None
-    
-    keys = str(key_path).split('.')
-    value = dictionary
-    
-    for key in keys:
-        if isinstance(value, dict) and key in value:
-            value = value[key]
-        else:
-            return None
-    return value
+@register.filter
+def get_item_int(dictionary, key):
+    """Get an item from a dictionary by integer key"""
+    return dictionary.get(int(key))
